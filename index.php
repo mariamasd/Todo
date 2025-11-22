433cff5dadb00d4cfe1b5fed3546972afacbc796
mariamasd <mariamasadou2307@gmail.com>
2025-11-22 14:37:12 +0100

    mis en place de l'application todo

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <?php require_once 'include/head.php' ?>
+    <title>Todo</title>
+</head>
+
+<body>
+    <div class="container">
+        <?php require_once 'include/header.php' ?>
+        <div class="content">
+            <div class="todo-container">
+                <h1>Ma Todo</h1>
+                <div class="todo-form"></div>
+                <div class="todo-list"></div>
+            </div>
+        </div>
+        <?php require_once 'include/footer.php' ?>
+    </div>
+</body>
+
+</html>
