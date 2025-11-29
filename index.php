870152b8f0104e3537a829218ec129fc76cc6c84
mariamasd <mariamasadou2307@gmail.com>
2025-11-29 17:48:43 +0100

    Implemetation du formulaire ,la gestion des erreur et la sauvegarde sur un fichier json

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
+<?php
+const ERROR_REQUIRED = 'Veuillez renseigner une todo';
+const ERROR_TOO_SHORT = 'Veuillez entrer au moins 5 caractères';
+$filename = __DIR__ . "/data/todos.json";
+$error = '';
+$todos = [];
+
+if (file_exists($filename)) {
+    $data = file_get_contents($filename);
+    $todos = json_decode($data, true) ?? [];
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $_POST = filter_input_array(INPUT_POST, [
+        "todo" => [
+            "filter" => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+            "flags" => FILTER_FLAG_NO_ENCODE_QUOTES | FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_BACKTICK
+        ]
+    ]);
+    $todo = $_POST['todo'] ?? '';
+
+    if (!$todo) {
+        $error = ERROR_REQUIRED;
+    } else if (mb_strlen($todo) < 5) {
+        $error = ERROR_TOO_SHORT;
+    }
+
+    if (!$error) {
+        $todos = [...$todos, [
+            'name' => $todo,
+            'done' => false,
+            'id' => time()
+        ]];
+        file_put_contents($filename, json_encode($todos, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -12,7 +50,13 @@
         <div class="content">
             <div class="todo-container">
                 <h1>Ma Todo</h1>
-                <div class="todo-form"></div>
+                <form class="todo-form" action="/" method="post">
+                    <input name="todo" type="text">
+                    <button class="btn btn-primary">Ajouter</button>
+                </form>
+                <?php if ($error) : ?>
+                    <p class="text-danger"><?= $error ?></p>
+                <?php endif; ?>
                 <div class="todo-list"></div>
             </div>
         </div>
