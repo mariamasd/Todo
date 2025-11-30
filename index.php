275ce68e0349603bb39cd30f029a03ff81278b8e
mariamasd <mariamasadou2307@gmail.com>
2025-11-30 01:46:50 +0100

    Ajout de la supression et de la validation du todo

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ const ERROR_REQUIRED = 'Veuillez renseigner une todo';
 const ERROR_TOO_SHORT = 'Veuillez entrer au moins 5 caractères';
 $filename = __DIR__ . "/data/todos.json";
 $error = '';
+$todo = '';
 $todos = [];
 
 if (file_exists($filename)) {
@@ -11,12 +12,7 @@ if (file_exists($filename)) {
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $_POST = filter_input_array(INPUT_POST, [
-        "todo" => [
-            "filter" => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
-            "flags" => FILTER_FLAG_NO_ENCODE_QUOTES | FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_BACKTICK
-        ]
-    ]);
+    $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $todo = $_POST['todo'] ?? '';
 
     if (!$todo) {
@@ -31,16 +27,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             'done' => false,
             'id' => time()
         ]];
-        file_put_contents($filename, json_encode($todos, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+        file_put_contents($filename, json_encode($todos));
+        $todo = '';
+        header('Location: /');
     }
 }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
-    <?php require_once 'include/head.php' ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <script async src="public/js/index.js"></script>
     <title>Todo</title>
 </head>
 
@@ -51,16 +56,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <div class="todo-container">
                 <h1>Ma Todo</h1>
                 <form class="todo-form" action="/" method="post">
-                    <input name="todo" type="text">
+                    <input value="<?= $todo ?>" name="todo" type="text">
                     <button class="btn btn-primary">Ajouter</button>
                 </form>
                 <?php if ($error) : ?>
                     <p class="text-danger"><?= $error ?></p>
                 <?php endif; ?>
-                <div class="todo-list"></div>
+                <ul class="todo-list">
+                    <?php foreach ($todos as $t) : ?>
+                        <li class="todo-item <?= $t['done'] ? 'low-opacity' : '' ?>">
+                            <span class="todo-name"><?= $t['name'] ?></span>
+                            <a href="/edit-todo.php?id=<?= $t['id'] ?>">
+                                <button class="btn btn-primary btn-small"><?= $t['done'] ? 'Annuler' : 'Valider' ?></button>
+                            </a>
+                            <a href="/remove-todo.php?id=<?= $t['id'] ?>">
+                                <button class="btn btn-danger btn-small">Supprimer</button>
+                            </a>
+
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
             </div>
+
         </div>
-        <?php require_once 'include/footer.php' ?>
+        <?php require_once 'includes/footer.php' ?>
     </div>
 </body>
 
